3ea10d38cfea53b9436986c982d647445adf5a8c
Drikus Roor <drikusroor@gmail.com>
2017-06-08 21:41:40 +0200

    Added a color class

---
+++ classes/colors.py
@@ -0,0 +1,11 @@
+class Colors():
+
+    def __init__():
+        self.red = (255, 0, 0)
+        self.green = (0, 255, 0)
+        self.blue = (0, 0, 255)
+        self.yellow = (255, 255, 0)
+        self.purple = (255, 0, 255)
+        self.turqoise = (0, 255, 255)
+        self.black = (0, 0, 0)
+        self.white = (255, 255, 255)
